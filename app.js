7c439e0faf44d8001d6fcd35f81f98edfc17b0ff
Sammy Roberts <omg.yeah.wai@hotmail.com>
2017-07-29 10:13:10 -0700

    comments and todo

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 /**
  * Created by sammyroberts on 7/24/17.
  */
+//google key is in my dotenv
 require("dotenv").config();
-const axios = require("axios");
-const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: true });
 
+//my http request lib TODO create a custom axios instance that automatically sends the headers for me.
+const axios = require("axios");
+
+const Nightmare = require('nightmare');
+const nightmare = Nightmare();
+
+//navigate to my site wait for the div to load, then get the important data
 nightmare.goto("http://www.bjjhq.com/")
 .wait(".right>h1")
 .evaluate(function () {
@@ -17,6 +22,8 @@ nightmare.goto("http://www.bjjhq.com/")
 .end()
 .then(function (result) {
     console.log(result);
+
+    //create my GCM request
     const data = {
         "to" : "/topics/bjjhq",
         "notification" : {
@@ -24,6 +31,8 @@ nightmare.goto("http://www.bjjhq.com/")
             "title" : "New deal from BJJHQ!",
         }
     };
+
+    //send GCM request - this creates the notification on my phone
     axios({
         url: "https://fcm.googleapis.com/fcm/send",
         method: "POST",
